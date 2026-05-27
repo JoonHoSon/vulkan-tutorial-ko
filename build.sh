e637ae76e08bf9dd207a07add6b8b5b963921e98
JoonHo Son <joonho.son@me.com>
2026-05-27 20:03:18 +0900

    Fragment shader 작성 중
    Drawing a triangle > Graphics pipeline basics > Introduction > Fragment shader

--- build.sh
+++ build.sh
@@ -6,7 +6,14 @@ fi
 
 cp -R ./assets ./build
 
+echo "Root documents build..."
 asciidoctor -a stylesheet=./assets/stylesheet.css -D ./build ./*.adoc
+
+echo "Setup documents build..."
 asciidoctor -a stylesheet=../assets/stylesheet.css -D ./build/Setup ./Setup/*.adoc
+
+echo "Presentation documents build..."
 asciidoctor -a stylesheet=../assets/stylesheet.css -D ./build/Presentation ./Presentation/*.adoc
+
+echo "GraphicsPipelineBasics documents build..."
 asciidoctor -a stylesheet=../assets/stylesheet.css -D ./build/GraphicsPipelineBasics ./GraphicsPipelineBasics/*.adoc
