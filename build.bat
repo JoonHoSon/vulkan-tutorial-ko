30db2d98cc725b7186e98862d047e5f90d1d9529
JoonHo Son <joonho.son@me.com>
2026-06-19 20:02:07 +0900

    Multisampling

--- build.bat
+++ build.bat
@@ -4,17 +4,17 @@ if not exist ".\build" md "build"
 
 robocopy ".\assets" ".\build\assets" /e
 
-echo "Root documents build..."
-asciidoctor -a stylesheet=.\assets\stylesheet.css -D .\build .\*.adoc
+echo "Root documents build..." 
+asciidoctor -a stylesheet=D:\dev\vulkan-tutorial\document\assets\stylesheet.css -D D:\dev\vulkan-tutorial\document\build D:\dev\vulkan-tutorial\document\*.adoc
 
-echo "Setup documents build..."
-asciidoctor -a stylesheet=..\assets\stylesheet.css -D .\build\Setup .\Setup\*.adoc
+echo "Setup documents build..." 
+asciidoctor -a stylesheet=D:\dev\vulkan-tutorial\document\assets\stylesheet.css -D D:\dev\vulkan-tutorial\document\build\Setup D:\dev\vulkan-tutorial\document\Setup\*.adoc
 
 echo "Presentation documents build..."
-asciidoctor -a stylesheet=..\assets\stylesheet.css -D .\build\Presentation .\Presentation\*.adoc
+asciidoctor -a stylesheet=D:\dev\vulkan-tutorial\document\assets\stylesheet.css -D D:\dev\vulkan-tutorial\document\build\Presentation D:\dev\vulkan-tutorial\document\Presentation\*.adoc
 
 echo "GraphicsPipelineBasics documents build..."
-asciidoctor -a stylesheet=..\assets\stylesheet.css -D .\build\GraphicsPipelineBasics .\GraphicsPipelineBasics\*.adoc
+asciidoctor -a stylesheet=D:\dev\vulkan-tutorial\document\assets\stylesheet.css -D D:\dev\vulkan-tutorial\document\build\GraphicsPipelineBasics D:\dev\vulkan-tutorial\document\GraphicsPipelineBasics\*.adoc
 
 echo "Drawing documents build..."
-asciidoctor -a stylesheet=..\assets\stylesheet.css -D .\build\Drawing .\Drawing\*.adoc
+asciidoctor -a stylesheet=D:\dev\vulkan-tutorial\document\assets\stylesheet.css -D D:\dev\vulkan-tutorial\document\build\Drawing D:\dev\vulkan-tutorial\document\Drawing\*.adoc
